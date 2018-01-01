995b2dab48de6abe39c2644ee679fb2a5f946c68
Pattnatt <jittapat007@gmail.com>
2018-01-01 14:31:51 +0700

    Add physics system to ball & pin. Create Pins Prefab.

---
+++ 10PinsBowling/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    public Ball ball;
+
+    public float cameraLimitZ = 1800;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - ball.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.z <= cameraLimitZ) {
+            transform.position = ball.transform.position + offset;
+        }
+        
+
+    }
+}
