8898727e42c144488607c96324d107b723877e2e
Pattnatt <jittapat007@gmail.com>
2018-01-03 17:31:59 +0700

    Add Pinsetter to dectect Pin's Behavior

--- 10PinsBowling/Assets/Scripts/Pin.cs
+++ 10PinsBowling/Assets/Scripts/Pin.cs
@@ -13,14 +13,14 @@ public class Pin : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        print(name + " " +  IsStanding());
+
     }
 
     public bool IsStanding () {
         Vector3 rotationInEuler = transform.rotation.eulerAngles;
         float tiltInX = Mathf.Abs(rotationInEuler.x);
         float tiltInZ = Mathf.Abs(rotationInEuler.z);
-        
+
         if (tiltInX > 180f) {
             tiltInX = 360f - tiltInX;
         }
---
+++ 10PinsBowling/Assets/Scripts/PinSetter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinSetter : MonoBehaviour {
+
+    public Text standingText;
+    private bool isBallEnter = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        standingText.text = CountStanding().ToString();
+
+    }
+
+    int CountStanding () {
+        int standing = 0;
+
+        foreach (Pin pin in GameObject.FindObjectsOfType<Pin>()) {
+            if (pin.IsStanding()) {
+                standing++;
+            }
+        }
+
+        return standing;
+    }
+
+    private void OnTriggerEnter (Collider collider) {
+        GameObject gameObject = collider.gameObject;
+        if (gameObject.GetComponent<Ball>()) {
+            isBallEnter = true;
+            print("Ball Entered");
+            standingText.color = Color.red;
+        }
+    }
+
+    private void OnTriggerExit (Collider collider) {
+        GameObject gameObject = collider.gameObject;
+        if (gameObject.GetComponentInParent<Pin>()) {
+            Destroy(gameObject.transform.parent.gameObject);
+        }
+    }
+}
