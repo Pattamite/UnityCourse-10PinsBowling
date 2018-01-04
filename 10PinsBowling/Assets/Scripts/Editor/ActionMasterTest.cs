4b71ae958e65cf34f4a2de4ac365f1760f623480
Pattnatt <jittapat007@gmail.com>
2018-01-04 14:21:22 +0700

    Create Simple Tests

---
+++ 10PinsBowling/Assets/Scripts/Editor/ActionMasterTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class ActionMasterTest {
+    private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+
+	[Test]
+	public void T00PassingTest() {
+        Assert.AreEqual(1, 1);
+	}
+
+	[Test]
+    public void T01OnStrikeReturnEndTurn () {
+        ActionMaster actionMaster = new ActionMaster();
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+}
